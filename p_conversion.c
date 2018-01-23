6854d676938115374d0e90ab6f8116afdb7498bf
Panyang Zhang <pzhang@e1z2r9p41.42.us.org>
2018-01-22 17:20:32 -0800

    p_conversion

--- p_conversion.c
+++ p_conversion.c
@@ -6,14 +6,54 @@
 /*   By: pzhang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 14:27:53 by pzhang            #+#    #+#             */
-/*   Updated: 2018/01/19 16:31:16 by pzhang           ###   ########.fr       */
+/*   Updated: 2018/01/22 17:20:01 by pzhang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	print_pad(int pad)
+{
+	while (pad > 0)
+	{
+		ft_putchar(' ');
+		pad--;
+	}
+}
+
+static int	helper(t_data *data)
+{
+	int	pad;
+
+	pad = 0;
+	if ((data->fw)[0] == 1 && (data->fw)[1] > 3)
+		pad = (data->fw)[1] - 3;
+	if (flag_check(data->f, '-') == 1)
+	{
+		ft_putstr("0x0");
+		print_pad(pad);
+	}
+	else
+	{
+		print_pad(pad);
+		ft_putstr("0x0");
+	}
+	return (3 + pad);
+}
+
 int			p_conversion(t_data *data, va_list ap)
 {
+	va_list			temp;
+	unsigned long	n;
+
+	va_copy(temp, ap);
+	n = va_arg(temp, unsigned long);
+	if (n == 0)
+	{
+		n = va_arg(ap, unsigned long);
+		va_end(temp);
+		return (helper(data));
+	}
 	set_flag(data, '#');
 	(data->p)[0] = 0;
 	(data->p)[1] = 0;
